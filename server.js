e30e4070b87e167cd2b214ec82e2c8bec0e06d12
Asmazabin Qureshi <Asmazabin.Qureshi@gatewaygroup.com>
2017-03-06 11:52:24 +0530

    hi

---
+++ server.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module('myApp', [])
+.controller('myctrl', function ($scope, $http) {	
+	$scope.toastMessage = "";	
+	$scope.submit = function () {
+		var saveDetail = {
+			'name': $scope.name,
+			'email': $scope.email,
+			'contactNum': $scope.contactNumber,
+			'message': $scope.message			
+		};	
+		$http.post("http://localhost:8000/api/saveRetailerDetails" , saveDetail).success(function (response) {		
+			if (response.status===201) { 
+				$scope.toastMessage = "Details successfully saved,We will get back to you soon.";
+				angular.element('#successMsg').stop().slideDown(1000).delay(1500).fadeOut(1000);					
+				contactForm.reset();
+					$scope.contactForm.$pristine = true;
+				$scope.contactForm.$submitted = false;
+			}
+			});
+	};	
+});
+
